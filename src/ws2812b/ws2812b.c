59d2bdf1989c9492128840406718ad11c8a72bdc
math-milan <lovis.ramthun@icatas.eu>
2025-02-25 00:45:57 +0100

    first working test version

--- src/ws2812b/ws2812b.c
+++ src/ws2812b/ws2812b.c
@@ -1,7 +1,7 @@
 #include "ws2812b.h"
 
 
-void ws2812b_init(ws2812b_inst *inst, uint8_t pin, uint number_of_leds){\
+void ws2812b_init(ws2812b_inst_t *inst, uint8_t pin, uint number_of_leds){\
     float freq = 800000;
     inst->number_of_leds = number_of_leds;
     uint offset = pio_add_program(PIO_INST, &ws2812_program);
@@ -9,7 +9,7 @@ void ws2812b_init(ws2812b_inst *inst, uint8_t pin, uint number_of_leds){\
     ws2812_program_init(PIO_INST, inst->state_maschine, offset, pin, freq, false);
 }
 
-void ws2812b_output_data(ws2812b_inst *inst, uint32_t buffer, int len){
+void ws2812b_output_data(ws2812b_inst_t *inst, uint32_t *buffer, int len){
     for (uint i = 0; i < len; i++)
     {
         pio_sm_put_blocking(PIO_INST, inst->state_maschine, buffer[i] << 8u);
--- src/ws2812b/ws2812b.h
+++ src/ws2812b/ws2812b.h
@@ -5,13 +5,13 @@
 
 #define PIO_INST pio0
 
-struct ws2812b_inst{
+typedef struct {
     uint state_maschine;
     uint number_of_leds;  
-};
+} ws2812b_inst_t;
 
-void ws2812b_init(ws2812b_inst *inst, uint8_t pin, uint number_of_leds);
-void ws2812b_output_data(ws2812b_inst *inst, uint32_t *buffer, int len);
+void ws2812b_init(ws2812b_inst_t *inst, uint8_t pin, uint number_of_leds);
+void ws2812b_output_data(ws2812b_inst_t *inst, uint32_t *buffer, int len);
 uint32_t ws2812b_convert_RGB(uint r, uint g, uint b);
 
 #endif
